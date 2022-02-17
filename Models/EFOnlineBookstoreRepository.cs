faf5aae7c790aef761c7f3cbf85256c2decea386
jenalmond <jencalmond@gmail.com>
2022-02-16 17:12:25 -0700

    Add project files.

---
+++ Models/EFOnlineBookstoreRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineBookstore.Models
+{
+    public class EFOnlineBookstoreRepository :IOnlineBookstoreRepository
+    {
+        private OnlineBookstoreContext context { get; set; }
+        public EFOnlineBookstoreRepository(OnlineBookstoreContext temp)
+        {
+            context = temp;
+        }
+        public IQueryable<Book> Books => context.Books;
+    }
+}
