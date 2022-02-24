dfe762c394179040c21f6ec6017658b7fbf9e11b
jenalmond <jencalmond@gmail.com>
2022-02-24 13:52:27 -0700

    Partial View working now.

---
+++ Components/ClassificationViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineBookstore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineBookstore.Components
+{
+    public class ClassificationViewComponent : ViewComponent
+    {
+        private IOnlineBookstoreRepository repo { get; set; }
+        public  ClassificationViewComponent (IOnlineBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var types = repo.Books
+                .Select(x => x.Classification)
+                .Distinct()
+                .OrderBy(x => x);
+            return View(types);
+        }
+    }
+}
