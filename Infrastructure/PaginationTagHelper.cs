45903db867d448e5dc4ca78e3f2a5bfdb051be22
jenalmond <jencalmond@gmail.com>
2022-02-23 21:31:42 -0700

    Button working....partial view not working

--- Infrastructure/PaginationTagHelper.cs
+++ Infrastructure/PaginationTagHelper.cs
@@ -26,7 +26,7 @@ namespace OnlineBookstore.Infrastructure
         public PageInfo PageModel { get; set; }
         public string PageAction { get; set; }
         public string PageClass { get; set; }
-        public bool PageClassEnabled { get; set; }
+        public bool PageClassesEnabled { get; set; }
         public string PageClassNormal { get; set; }
         public string PageClassSelected { get; set; }
         public override void Process (TagHelperContext thc, TagHelperOutput tho)
@@ -37,7 +37,7 @@ namespace OnlineBookstore.Infrastructure
             {
                 TagBuilder tb = new TagBuilder("a");
                 tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
-                if (PageClassEnabled)
+                if (PageClassesEnabled)
                 {
                     tb.AddCssClass(PageClass);
                     tb.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
--- Startup.cs
+++ Startup.cs
@@ -44,6 +44,11 @@ namespace OnlineBookstore
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                    name: "Paging",
+                    pattern: "Page{pageNum}",
+                    defaults: new { Controller = "Home", action = "Index" });
+
                 endpoints.MapDefaultControllerRoute();
             });
         }
