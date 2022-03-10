1d2f25b88bcc84dab1098a06e8d127fa9875209f
jenalmond <jencalmond@gmail.com>
2022-03-09 20:50:28 -0700

    Almost done with 2nd set of videos

---
+++ Migrations/20220308231842_updated.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineBookstore.Migrations
+{
+    public partial class updated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "TransactionReceived",
+                table: "Transactions",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TransactionReceived",
+                table: "Transactions");
+        }
+    }
+}
--- Models/IOnlineBookstoreRepository.cs
+++ Models/IOnlineBookstoreRepository.cs
@@ -8,5 +8,8 @@ namespace OnlineBookstore.Models
     public interface IOnlineBookstoreRepository
     {
         IQueryable<Book> Books { get; }
+        public void SaveBook(Book b);
+        public void CreateBook(Book b);
+        public void DeleteBook(Book b);
     }
 }
